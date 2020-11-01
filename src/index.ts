750128cacf9e57ea5909b589553d9de31e29ccc1
Gianluca Arbezzano <gianarb92@gmail.com>
2020-11-01 16:57:11 +0100

    First wrapper

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {ExtensionContext, services, workspace, LanguageClient, TransportKind} from 'coc.nvim'
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const config = workspace.getConfiguration().get<any>('grammarly', {}) as any
+  if (!config.enable) return
+  const serverOptions = {
+    // TODO: Somehow this has to be embedded
+    module: "/Users/gianarb/git/grammarly/out/server.js",
+    transport: TransportKind.ipc,
+    options: {
+      cwd: workspace.root,
+      execArgv: config.execArgv || []
+    }
+  }
+  const clientOptions = {
+    documentSelector: ['markdown', 'md'],
+  }
+  const client = new LanguageClient(
+    'coc-grammarly', // the id
+    'coc-grammarly', // the name of the language server
+    serverOptions,
+    clientOptions
+  )
+  context.subscriptions.push(services.registLanguageClient(client))
+}
+
+
